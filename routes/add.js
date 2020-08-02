3ee03ce9fb87100b2b05dfb5a8d26cabbf70280f
Belendor <dwarf11@gmail.com>
2020-08-02 19:45:56 +0300

    validation

--- routes/add.js
+++ routes/add.js
@@ -6,8 +6,12 @@ const authenticateToken = require("../middlweres/authenticateToken.js")
 
 router.post('/add', authenticateToken, logger,  (req, res)=>{ // adds new order.
 
+    if(!req.body.amount) return  res.status(201).send("Amount field is empy")
+    if(!req.body.currency) return res.status(201).send("Currency field is empty")
+    if(isNaN(req.body.currency && req.body.currency != true && req.body.currency != null)) return res.status(201).send("Currency must ne number")
+
     const newOrder = new Order({
-        amount: req.body.amount,
+        amount: req.body.amount, 
         currency: req.body.currency,
         addedby: req.user
     })
